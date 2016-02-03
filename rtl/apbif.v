3a85b2aa55d151fd631f203fd0dbe49ae0f97d8c
angie francia <apfrancia@gmail.com>
2016-02-03 18:51:46 +0800

    Add register_file for settings

--- rtl/apbif.v
+++ rtl/apbif.v
@@ -1,61 +1,76 @@
-module apbif (
-    output reg [31:0] 	O_PRDATA,
+`timescale 1ns/1ps
 
+module apbif (
+    output reg 	[31:0] 	O_PRDATA,
+    output reg 		O_PREADY,
+    
     input 		I_PSEL,
     input 		I_PENABLE,
-    input [31:0] 	I_PADDR,
     input 		I_PWRITE,
-    input [31:0] 	I_PWDATA,
+    input 	[31:0] 	I_PADDR,
+    input 	[31:0] 	I_PWDATA,
 
-    input 		I_PRESET_N
-    input 		I_PCLK,
+    input 		I_PRESET_N,
+    input 		I_PCLK
 );
 
-reg [1:0] curr_state;
-reg [1:0] next_state;
+integer i;
 
-//registers
-reg [
+reg [7:0] REGISTER_FILE [59:0];
 
-parameter 	IDLE	= 2'h0,
-		SETUP 	= 2'h1,
-		ENABLE	= 2'h2;
+wire [31:0] address;
+
+assign address = {I_PADDR[31:0],2'h0};
 
 always @(posedge I_PCLK)
     if (!I_PRESET_N)
-	curr_state <= IDLE;
+	O_PREADY <= 0;
     else 
-	curr_state <= next_state;
+	if ((I_PSEL && !I_PENABLE) || (I_PENABLE && !I_PSEL))
+	    O_PREADY <= 1;
+	else
+	    O_PREADY <= 0;
 
-always @(*)
+always @(posedge I_PCLK)
     if (!I_PRESET_N)
-	next_state <= IDLE;
+	for (i = 0; i < 60; i = i + 1)
+	    begin
+		REGISTER_FILE[i] <= 8'h00;
+	    end
     else 
-	case (curr_state)
-	    IDLE:
-		if (I_PSEL)
-		    next_state <= SETUP;
-		else 
-		    next_state <= IDLE;
-	    SETUP:
-		if (I_PENABLE)
-		    next_state <= ENABLE;
-		else 
-		    next_state <= SETUP;
-	    ENABLE:
-		if (I_PSEL)
-		    next_state <= SETUP;
-		else 
-		    next_state <= IDLE;
-	endcase
+	if (I_PSEL && I_PENABLE && I_PWRITE)
+	    for (i = 0; i < 60; i = i + 4)
+		begin
+		    if (i == address)
+			begin
+			    REGISTER_FILE[i] 	<= I_PWDATA[7:0];
+			    REGISTER_FILE[i + 1] 	<= I_PWDATA[15:8];
+			    REGISTER_FILE[i + 2] 	<= I_PWDATA[23:16];
+			    REGISTER_FILE[i + 3] 	<= I_PWDATA[31:24];
+			end
+		    else
+			begin
+			    REGISTER_FILE[i] 	<= REGISTER_FILE[i];    
+			    REGISTER_FILE[i + 1] 	<= REGISTER_FILE[i + 1]; 
+			    REGISTER_FILE[i + 2] 	<= REGISTER_FILE[i + 2];
+			    REGISTER_FILE[i + 3] 	<= REGISTER_FILE[i + 3];
+			end
+		end
+	else
+	    for (i = 0; i < 60; i = i + 1)
+		begin
+		    REGISTER_FILE[i] <= REGISTER_FILE[i];
+		end
 
-always @(posedge I_HCLK)
+always @(posedge I_PCLK)
     if (!I_PRESET_N)
-	data <= 32'h00000000;
-    else
-	if (!I_PWRITE)
-	    data <= regdata_out;
-	else 
-	    data <= data;
+	O_PRDATA <= 32'h00000000;
+    else 
+	begin
+	    O_PRDATA[7:0] <= REGISTER_FILE[address];
+	    O_PRDATA[15:8] <= REGISTER_FILE[address + 1];
+	    O_PRDATA[23:26] <= REGISTER_FILE[address + 2];
+	    O_PRDATA[31:24] <= REGISTER_FILE[address + 3];
+	end
 
 endmodule
---
+++ rtl/tb_apb.v
@@ -0,0 +1,86 @@
+`timescale 1ns/1ps
+
+module tb_apb;
+
+
+    wire [31:0] O_PRDATA;
+    wire 	O_PREADY;
+    
+    reg 	I_PSEL;
+    reg 	I_PENABLE;
+    reg 	I_PWRITE;
+    reg [31:0] 	I_PADDR;
+    reg [31:0] 	I_PWDATA;
+    reg 	I_PRESET_N;
+    reg 	I_PCLK;
+
+    apbif APB0 (
+    .O_PRDATA(O_PRDATA),
+    .O_PREADY(O_PREADY), 
+    .I_PSEL(I_PSEL),
+    .I_PENABLE(I_PENABLE),
+    .I_PWRITE(I_PWRITE),
+    .I_PADDR(I_PADDR),
+    .I_PWDATA(I_PWDATA),
+    .I_PRESET_N(I_PRESET_N),
+    .I_PCLK(I_PCLK)
+    );
+
+    initial begin
+	I_PCLK = 0; 
+	I_PRESET_N = 1;
+	I_PSEL = 0;
+	I_PENABLE = 0;
+	I_PWRITE = 0;
+	I_PADDR = 0;
+	I_PWDATA = 0;
+    end
+
+    always 
+	#1 I_PCLK = ~I_PCLK;
+
+    initial begin
+	$vcdpluson;
+	$vcdplusmemon;
+
+	@(posedge I_PCLK)
+	    I_PRESET_N <= 1;
+
+	@(posedge I_PCLK)
+	    I_PRESET_N <= 0;
+
+	@(posedge I_PCLK)
+	    begin
+		I_PADDR <= 0;
+		I_PWDATA <= 0;
+	    end
+
+	@(posedge I_PCLK)
+	    begin
+		I_PADDR <= 4;
+		I_PWDATA <= 500;
+	    end
+
+	@(posedge I_PCLK)
+	    I_PSEL <= 1;
+
+	@(posedge I_PCLK)
+	    I_PENABLE <= 1;
+
+	@(posedge I_PCLK)
+	    I_PENABLE <= 0;
+
+	@(posedge I_PCLK)
+	    I_PWRITE <= 1;
+
+	@(posedge I_PCLK)
+	    begin
+		I_PADDR <= 4;
+		I_PWDATA <= 128;
+		I_PENABLE <= 1;
+	    end
+
+	#500 $finish;
+    end
+
+endmodule
