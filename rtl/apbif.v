f1f6a299ad6e75565727ee5444124c2f5396245d
angie francia <apfrancia@gmail.com>
2016-02-04 19:15:40 +0800

    Add register_file signals for core and DMA

--- rtl/apbif.v
+++ rtl/apbif.v
@@ -3,7 +3,24 @@
 module apbif (
     output reg 	[31:0] 	O_PRDATA,
     output reg 		O_PREADY,
-    
+    output reg 		O_INTERRUPT,
+
+    output 	[31:0] 	O_DMA_SRC_IMG,
+    output 	[31:0] 	O_DMA_DST_IMG,
+    output 	[15:0] 	O_ROT_IMG_H,
+    output 	[15:0] 	O_ROT_IMG_W,
+    output 	[15:0] 	O_ROT_IMG_NEW_H,
+    output 	[15:0] 	O_ROT_IMG_NEW_W,
+    output 	[1:0] 	O_ROT_IMG_MODE,
+    output  		O_ROT_IMG_DIR,
+    output  		O_CTRL_START,
+    output  		O_CTRL_RESET,
+    output 		O_CTRL_INTR_MASK,
+    output 		O_CTRL_BEF_MASK,	// TODO: confirm if read by core
+    output 		O_CTRL_AFT_MASK,	// TODO: confirm if read by core
+    output 		O_CTRL_INTR_CLEAR,
+    output 		O_CTRL_BUSY,		// TODO: confirm if read by core
+
     input 		I_PSEL,
     input 		I_PENABLE,
     input 		I_PWRITE,
@@ -28,6 +45,7 @@ assign address2 = address1 + 6'h01;
 assign address3 = address1 + 6'h02;
 assign address4 = address1 + 6'h03;
 
+//request master to extend read state
 always @(posedge I_PCLK)
     if (!I_PRESET_N)
 	O_PREADY <= 0;
@@ -37,6 +55,8 @@ always @(posedge I_PCLK)
 	else
 	    O_PREADY <= 0;
 
+// TODO: change to case statement for self-clearing & non-writeable registers
+// write to REGISTER_FILE
 always @(posedge I_PCLK)
     if (!I_PRESET_N)
 	for (i = 0; i < 60; i = i + 1)
@@ -53,6 +73,8 @@ always @(posedge I_PCLK)
 	    for (j = 0; j < 60; j = j + 1)
 		REGISTER_FILE[j] <= REGISTER_FILE[j];
 
+// TODO: change to case statement for non-readable registers
+// to CPU
 always @(posedge I_PCLK)
     if (!I_PRESET_N)
 	O_PRDATA <= 32'h00000000;
@@ -61,10 +83,27 @@ always @(posedge I_PCLK)
 	    begin
 		O_PRDATA[7:0] 	<= REGISTER_FILE[address1];
 		O_PRDATA[15:8] 	<= REGISTER_FILE[address2];
-		O_PRDATA[23:16] 	<= REGISTER_FILE[address3];
-		O_PRDATA[31:24] 	<= REGISTER_FILE[address4];
+		O_PRDATA[23:16]	<= REGISTER_FILE[address3];
+		O_PRDATA[31:24]	<= REGISTER_FILE[address4];
 	    end
 	else 
 	    O_PRDATA <= O_PRDATA;
+
+//to core
+assign O_DMA_SRC_IMG = {REGISTER_FILE[6'h03],REGISTER_FILE[6'h02],REGISTER_FILE[6'h01],REGISTER_FILE[6'h00]};
+assign O_DMA_DST_IMG = {REGISTER_FILE[6'h07],REGISTER_FILE[6'h06],REGISTER_FILE[6'h05],REGISTER_FILE[6'h04]};
+assign O_ROT_IMG_H = {REGISTER_FILE[6'h09],REGISTER_FILE[6'h08]};
+assign O_ROT_IMG_W = {REGISTER_FILE[6'h0d],REGISTER_FILE[6'h0c]};
+assign O_ROT_IMG_NEW_H = {REGISTER_FILE[6'h11],REGISTER_FILE[6'h10]};
+assign O_ROT_IMG_NEW_W = {REGISTER_FILE[6'h15],REGISTER_FILE[6'h14]};
+assign O_ROT_IMG_MODE = REGISTER_FILE[6'h18][1:0]; 
+assign O_ROT_IMG_DIR = REGISTER_FILE[6'h1c][0];
+assign O_CTRL_START = REGISTER_FILE[6'h20][0];
+assign O_CTRL_RESET = REGISTER_FILE[6'h24][0];
+assign O_CTRL_INTR_MASK = REGISTER_FILE[6'h28][0];
+assign O_CTRL_BEF_MASK = REGISTER_FILE[6'h2c][0];
+assign O_CTRL_AFT_MASK = REGISTER_FILE[6'h30][0];
+assign O_CTRL_INTR_CLEAR = REGISTER_FILE[6'h34][0];
+assign O_CTRL_BUSY = REGISTER_FILE[6'h38][0];
 
 endmodule
